f73c3e283f3bbdd1403dde61e145b95f6e517eb9
Anwari Ilman <anwari.ilman@gmail.com>
2018-12-18 12:48:22 +0000

    restructured the app and it's able to create and remove some feeds

--- assets/script.js
+++ assets/script.js
@@ -5,26 +5,32 @@
     var feedUrl = 'https://www.engadget.com/rss-full.xml';
     var $FeedItemTemplate = $('#rss-item-template');
     var $NewFeedTemplate = $('#new-feedreader');
+    var $feedHeaderTemplate = $('#rss-item-heading');
     var $loadingTemplate = $('#loading-feed');
+    var $errorFeedTemplate = $('#error-feed');
     var $feedsContainer = $('#feed-reader-container');
+    var colors = ['#e90052', '#02b35e', '#f90000', '#f95202', '#4f6bef'];
 
+    // The FeedReader constructor to initiate new object when called.
+    // Every instance inherits prototypal inheritance
     var FeedReader = function(url, callback){
         this.id         = Date.now();
         this.url        = url || false;
         this.callback   = callback || function(){ };
         this.num        = 10;
+        this.color      = colors[ feeds.length % colors.length];
         // Since engadget's server does not come with X-ORIGIN REQUEST POLICY, then 
         // I'm using a service from feedrapp that provide such service
         this.serviceName= 'www.feedrapp.info';
         this.$htmlContainer = $('<div id="rss-items-container-' 
                                     + this.id 
-                                    + '" class="rss-items-container col-12 col-md-6 col-xl-4 my-3 p-3 bg-white rounded shadow-sm">');
+                                    + '" class="rss-items-container col-12 col-md-6">\
+                                        <div class="m-1 p-3 bg-white rounded shadow-lg"></div>\
+                                    </div>');
 
-        // logic to render the container + create new
         
-
         if (url) { this.loadFeed() } 
-        else { this.create() } 
+            else { this.create() } 
         
         this.renderContainer();
     }
@@ -35,15 +41,23 @@
         this.$NewFeedTemplate = $NewFeedTemplate.clone();
 
         this.$NewFeedTemplate
+                .find('form')
                 .on('submit', function(e){ e.preventDefault(); _this.url = _this.$NewFeedTemplate.find('#feed-url').val(); _this.loadFeed() } )
-                .on('click', '#cancel-feed', function(e){ _this.destroy() } ); // !!
+                .on('click', '#cancel-feed', function(e){ e.preventDefault(); _this.destroy() } ); // !!
 
         this.render( this.$NewFeedTemplate );
     }
 
     FeedReader.prototype.destroy = function(){
-        this.$htmlContainer.remove();
-        // need to remove from feeds as well.
+        var _this = this;
+        this.$htmlContainer.addClass('mySlideOutLeft');
+
+        setTimeout(function(){ 
+            _this.$htmlContainer.remove(); 
+            feeds.pop(); // !warning `feeds` is mutated
+            (feeds.length < 2) && $feedsContainer.children().removeClass('col-xl-4');
+        }, 1000);
+        
     }
 
     FeedReader.prototype.formatNewFeedTemplate = function(){
@@ -52,7 +66,13 @@
 
     // put the whole thing into the DOM.
     FeedReader.prototype.renderContainer = function(){
-        $feedsContainer.prepend( this.$htmlContainer );
+
+        if ( feeds.length >= 2 ) {
+            $feedsContainer.children().addClass('col-xl-4')
+            this.$htmlContainer.addClass('col-xl-4')
+        }
+
+        $feedsContainer.prepend( this.$htmlContainer.addClass('animated mySlideInLeft') );
     }
 
     FeedReader.prototype.fetch = function(callback){
@@ -71,6 +91,12 @@
         
         try {
             this.fetch( function(data){
+                
+                if ( data.responseData.feed === null ) {
+                    _this.renderError();
+                    return;
+                }
+
                 _this.feed = Object.assign({}, data.responseData.feed);
                 _this.entries = Object.assign({}, data.responseData.feed.entries);
                 
@@ -81,7 +107,7 @@
             })
         } 
         catch (e) {
-                console.log("Error cuy.");
+                console.log("Error.");
         }
     }
 
@@ -105,13 +131,13 @@
     FeedReader.prototype.formatEntry = function(entry){
         var $entryTemplate = $FeedItemTemplate.clone();
 
-        
         // find the items and replace with respective attribute
         $entryTemplate.attr( 'id', 'rss-'+ this.id + '-item-' + entry.number );
         $entryTemplate.find('.number').text( entry.number );
         $entryTemplate.find('.item-title').text( entry.title );
         $entryTemplate.find('.excerpt').text(  this.generateExcerpt( entry.content ) );
         $entryTemplate.find('.readmore').prop( 'href', entry.link ).attr('title', entry.title );
+        $entryTemplate.find('.author').text( entry.author && ' | ' + entry.author );
         $entryTemplate.find('.published-date').text( this.formatPublishedDate( entry.publishedDate ) );
         $entryTemplate.css('display', 'inherit');
 
@@ -121,12 +147,25 @@
 
     // Format the header. Ideally with RSS info like the name, desc, etc.
     FeedReader.prototype.formatHeader = function(){
-        return $('<h4 class="pb-6 mb-0">' + this.feed.title + '</h4>');
+        this.$header = $feedHeaderTemplate.clone();
+        this.$header.text( this.feed.title );        
+
+        return this.$header;
     }
 
+    FeedReader.prototype.renderError = function(){
+        var _this = this;
+        this.$errorFeed = $errorFeedTemplate.clone();
+        this.$errorFeed
+            .find('.back-to-create-feed')
+            .click(function(e){ e.preventDefault()
+                    _this.render( _this.$NewFeedTemplate );
+            });
+        this.render( this.$errorFeed );
+    }
 
     FeedReader.prototype.render = function( $item ){
-        this.$htmlContainer.html( $item )
+        this.$htmlContainer.find('.bg-white').html( $item )
     }
 
     FeedReader.prototype.refresh = function(){
@@ -147,7 +186,7 @@
                 + '...';
     }
 
-    // a utility funciton to decode HTML entities
+    // a utility function to decode HTML entities
     function htmlDecode(input) {
         var doc = new DOMParser().parseFromString(input, "text/html");
         return doc.documentElement.textContent;
@@ -156,12 +195,11 @@
     function bindEvents() {
         $('#add-new-feed').on('click', function(e){
             var reader = new FeedReader();
-    
             feeds.push( reader );
         });
     }
 
-    // Main function
+    // Main function.
     function main(){
         bindEvents();
 
